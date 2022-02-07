21b02a584aa8e5dc9fff433dac855265f62dabb1
Aqdas Malik <bilalahmer1995@gmail.com>
2022-02-07 19:03:05 +0500

    fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,9 @@ Rails.application.configure do
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
-    :enable_starttls_auto => true
+    :enable_starttls_auto => true,
+    open_timeout:         5,
+    read_timeout:         5
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
