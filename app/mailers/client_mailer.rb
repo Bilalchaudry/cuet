6dbc1089b7dae94477d112cb2422f59bc0768d0e
Aqdas Malik <bilalahmer1995@gmail.com>
2022-02-07 17:30:57 +0500

    email

--- app/mailers/client_mailer.rb
+++ app/mailers/client_mailer.rb
@@ -1,5 +1,5 @@
 class ClientMailer < ApplicationMailer
-  default from: 'ceutisolpharma@gmail.com'
+  default from: 'bilalahmer1995@gmail.com'
 
   def order_email_to_client(order)
     @user = params[:user]
