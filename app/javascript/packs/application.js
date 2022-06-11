f6fcb754d57d0219a07cada8f87e591cd1c94d2d
Bilal Ahmad <bilalahmer1995@gmail.com>
2022-06-11 15:51:26 +0500

    import crousal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("slick-carousel")
+import "slick-carousel";
 import "slick-carousel/slick/slick.scss"
 import "slick-carousel/slick/slick-theme.scss"
 window.jQuery = $;
