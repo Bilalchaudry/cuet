8d29d9a96d5a268f00921a00c28fbbf38a8bf8e4
Bilal Ahmad <32307474+Bilalchaudry@users.noreply.github.com>
2024-11-19 20:20:55 +0500

    updates

--- app/models/cert.rb
+++ app/models/cert.rb
@@ -2,4 +2,9 @@ class Cert < ApplicationRecord
   belongs_to :client
   has_many :cert_products, dependent: :destroy
 
+  def self.ransackable_attributes(auth_object = nil)
+    ["client_id", "created_at", "id", "id_value", "ordered?", "updated_at"]
+  end
+
+
 end
--- app/models/slide_image.rb
+++ app/models/slide_image.rb
@@ -1,3 +1,7 @@
 class SlideImage < ApplicationRecord
   mount_uploader :image, PhotoUploader
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "id_value", "image", "publish", "updated_at"]
+  end
 end
